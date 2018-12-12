667ceeef1c8a54bd1fcf46bc6fe383501ea1a4ca
Jesslyn Starnes <jesslyn.starnes@wsu.edu>
2018-12-12 17:22:06 -0500

    Create moving_average_test.py

---
+++ data_analysis/moving_average_test.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+from .data analysis import moving_average
+
+
+
+def test_moving_avg():
+	avg = moving_average(np.ones(1000), 2)
+	assert np.all(np.isnan(avg[0:2]))
+	assert np.all(np.isnan(avg[-2:]))
+	assert np.allclose(avg[2:-2], 1)
